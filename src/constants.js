dd9680d83588148595cf6cd25b1489fdea32228a
Debarati Datta <debaratidatta.77star@gmail.com>
2025-01-26 03:36:15 -0800

    update marvel link

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ import { proxy } from 'valtio';
 export const constants= {
     "MARVEL_PUBLIC":"ae418c63168ded3f297ae602c8b4231a",
     "MARVEL_PRIVATE":"2f0bef067292eb85a54b91dc859e57e0cb562b27",
-    "MARVEL_URL":"http://gateway.marvel.com"
+    "MARVEL_URL":"https://gateway.marvel.com"
 }
 
 export const heroStore = proxy({
